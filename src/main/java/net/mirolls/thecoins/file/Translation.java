1d170908dfa6262e6e09803e7d1683d6720488c0
liangmiQwQ <mi_yyds@126.com>
2024-10-28 20:57:17 +0800

    feat: add the translation

--- src/main/java/net/mirolls/thecoins/file/Translation.java
+++ src/main/java/net/mirolls/thecoins/file/Translation.java
@@ -1,16 +1,22 @@
 package net.mirolls.thecoins.file;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import net.mirolls.thecoins.TheCoins;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.Map;
+import java.util.HashMap;
 
 public class Translation {
-  private static Map<String, String> cachedTranslation;
-  private static Map<String, String> cachedPlayerConfig;
+  private final String lang;
+  private HashMap<String, String> cachedTranslation;
 
-  private static void createConfig() {
+  Translation(String lang) {
+    this.lang = lang;
+  }
+
+  private static void createTranslationFile() {
     String defaultZhCnConfigFile = ResourceReader.readResourceAsString("zh_cn.json");
     String defaultEnUsConfigFile = ResourceReader.readResourceAsString("en_us.json");
 
@@ -65,4 +71,43 @@ public class Translation {
     }
   }
 
+  private static boolean isTranslationFileHere() {
+    File bettertipsFolder = new File(new File("."), "skyblock");
+    if (!bettertipsFolder.exists()) {
+      return false;
+    }
+    File zhCnFile = new File(bettertipsFolder, "zh_cn.json");
+    File enUsFile = new File(bettertipsFolder, "en_us.json");
+    return zhCnFile.exists() && enUsFile.exists();
+  }
+
+  public static void initTranslation() {
+    if (!isTranslationFileHere()) {
+      // 如果没有则创建，反正保证的就是一个有文件
+      createTranslationFile();
+    }
+  }
+
+  private static HashMap<String, String> getTranslationFile(String filename) throws IOException {
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    return objectMapper.readValue(
+        new File(new File(new File("."), "skyblock"), filename),
+        new TypeReference<>() {
+        }
+    );
+  }
+
+  public String getTranslation(String translateID) {
+    initTranslation();
+
+    if (cachedTranslation.isEmpty()) {
+      try {
+        cachedTranslation = getTranslationFile(lang + ".json");
+      } catch (IOException e) {
+        TheCoins.LOGGER.error("Cannot read language file" + e);
+      }
+    }
+    return cachedTranslation.get(translateID);
+  }
 }
