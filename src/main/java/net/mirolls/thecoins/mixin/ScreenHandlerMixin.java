cd4a2f70adb976419a74832df1a46f7a58470059
liangmiQwQ <mi_yyds@126.com>
2024-11-12 20:24:58 +0800

    feat: fix something

--- src/main/java/net/mirolls/thecoins/mixin/ScreenHandlerMixin.java
+++ src/main/java/net/mirolls/thecoins/mixin/ScreenHandlerMixin.java
@@ -6,12 +6,12 @@ import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.screen.slot.SlotActionType;
 import net.minecraft.util.collection.DefaultedList;
-import net.mirolls.thecoins.TheCoins;
 import net.mirolls.thecoins.gui.SKBMenu;
 import net.mirolls.thecoins.menu.Menu;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -22,13 +22,18 @@ public abstract class ScreenHandlerMixin {
   @Shadow
   @Final
   public DefaultedList<Slot> slots;
-//  private boolean canSWAP = false;
+
+  @Unique
+  private int setStackButton;
+  @Unique
+  private ItemStack setStackItemStack;
+
 
   @Shadow
   public abstract Slot getSlot(int index);
 
   @Inject(method = "onSlotClick", at = @At("HEAD"), cancellable = true)
-  public void onSlotClick(int slotIndex, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci) {
+  public void onSlotClickHead(int slotIndex, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci) {
     if (slotIndex != -999 && actionType != SlotActionType.SWAP) {
       ItemStack stack = getSlot(slotIndex).getStack();
       if (Menu.isMenu(stack)) {
@@ -38,19 +43,26 @@ public abstract class ScreenHandlerMixin {
     }
   }
 
+  @Inject(method = "onSlotClick", at = @At("TAIL"))
+  public void onSlotClickReturn(int slotIndex, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci) {
+    if (actionType == SlotActionType.SWAP) {
+      player.getInventory().setStack(this.setStackButton, this.setStackItemStack);
+    }
+  }
+
+
   @Inject(method = "internalOnSlotClick", at = @At("HEAD"), cancellable = true)
-  private void internalOnSlotClick(int slotIndex, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci) {
+  private void internalOnSlotClick(int slotIndex, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci) throws InterruptedException {
     if (actionType == SlotActionType.SWAP && (button >= 0 && button < 9 || button == 40)) { //0-9 bar or offhand
       ItemStack itemStackInSlot = player.getInventory().getStack(button);
       Slot slot = this.slots.get(slotIndex);
       ItemStack itemStackInHand = slot.getStack();
-      TheCoins.LOGGER.info("SlotItem" + itemStackInSlot + "HandItem" + itemStackInHand);
-      if (Menu.isMenu(itemStackInSlot)) {
-        slot.setStack(itemStackInHand);
+      if (Menu.isMenu(itemStackInSlot) || Menu.isMenu(itemStackInHand)) {
+        // slot is menu
+        // we should replace itemStackInSlot (replace menu)
         ci.cancel();
-      } else if (Menu.isMenu(itemStackInHand)) {
-        player.getInventory().setStack(button, itemStackInSlot);
-        ci.cancel();
+        this.setStackButton = button;
+        this.setStackItemStack = itemStackInSlot;
       }
     }
   }
