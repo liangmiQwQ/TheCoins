1c5e3db137650e8e61bcb965eb24f51695b1c2ff
liangmiQwQ <mi_yyds@126.com>
2024-11-19 06:02:36 +0800

    fix: do not crash at simple SWAP

--- src/main/java/net/mirolls/thecoins/mixin/ScreenHandlerMixin.java
+++ src/main/java/net/mirolls/thecoins/mixin/ScreenHandlerMixin.java
@@ -6,6 +6,7 @@ import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.screen.slot.SlotActionType;
 import net.minecraft.util.collection.DefaultedList;
+import net.mirolls.thecoins.TheCoins;
 import net.mirolls.thecoins.gui.SKBMenu;
 import net.mirolls.thecoins.item.Menu;
 import org.spongepowered.asm.mixin.Final;
@@ -45,7 +46,7 @@ public abstract class ScreenHandlerMixin {
 
   @Inject(method = "onSlotClick", at = @At("TAIL"))
   public void onSlotClickReturn(int slotIndex, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci) {
-    if (actionType == SlotActionType.SWAP) {
+    if (actionType == SlotActionType.SWAP && !player.getWorld().isClient && this.setStackButton != -999 && this.setStackItemStack != null) {
       player.getInventory().setStack(this.setStackButton, this.setStackItemStack);
     }
   }
@@ -53,18 +54,21 @@ public abstract class ScreenHandlerMixin {
 
   @Inject(method = "internalOnSlotClick", at = @At("HEAD"), cancellable = true)
   private void internalOnSlotClick(int slotIndex, int button, SlotActionType actionType, PlayerEntity player, CallbackInfo ci) throws InterruptedException {
-    if (actionType == SlotActionType.SWAP && (button >= 0 && button < 9 || button == 40)) { //0-9 bar or offhand
+    if (actionType == SlotActionType.SWAP && (button >= 0 && button < 9 || button == 40) && !player.getWorld().isClient) { //0-9 bar or offhand
       ItemStack itemStackInSlot = player.getInventory().getStack(button);
       Slot slot = this.slots.get(slotIndex);
       ItemStack itemStackInHand = slot.getStack();
+      TheCoins.LOGGER.info("itemStackInSlot: " + itemStackInSlot + " itemStackInHand: " + itemStackInHand);
       if (Menu.isMenu(itemStackInSlot) || Menu.isMenu(itemStackInHand)) {
         // slot is menu
         // we should replace itemStackInSlot (replace menu)
         ci.cancel();
         this.setStackButton = button;
         this.setStackItemStack = itemStackInSlot;
+        return;
       }
     }
+    this.setStackButton = -999;
   }
 
 }
