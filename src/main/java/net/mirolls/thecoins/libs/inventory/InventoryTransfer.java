a8b412e0ab7a31d82b8656ec4702b0c3874c5010
liangmiQwQ <mi_yyds@126.com>
2024-12-07 10:12:01 +0800

    feat: add deserializer of inventories

--- src/main/java/net/mirolls/thecoins/libs/inventory/InventoryTransfer.java
+++ src/main/java/net/mirolls/thecoins/libs/inventory/InventoryTransfer.java
@@ -2,11 +2,14 @@ package net.mirolls.thecoins.libs.inventory;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.inventory.EnderChestInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtHelper;
 import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.collection.DefaultedList;
 import net.mirolls.thecoins.TheCoins;
 
@@ -35,6 +38,38 @@ public class InventoryTransfer {
     }
   }
 
+  public static void setPlayerInventoryFromJSON(String inventoryString, PlayerInventory inventory) {
+    // parse inventory JSON
+    ObjectMapper objectMapper = new ObjectMapper();
+    PlayerInventoryJsonParser playerInventory;
+    try {
+      playerInventory = objectMapper.readValue(
+          inventoryString,
+          objectMapper.getTypeFactory().constructType(PlayerInventoryJsonParser.class)
+      );
+    } catch (JsonProcessingException e) {
+      TheCoins.LOGGER.error("Cannot get the inventory of player " + e);
+      throw new RuntimeException(e);
+    }
+
+    // clear
+    inventory.clear();
+
+    // start to set main
+    for (int index : playerInventory.getMain().keySet()) {
+      // get itemStack
+      inventory.setStack(index, transferInventoryToItemStack(playerInventory.getMain().get(index)));
+    }
+    // set armor
+    for (int index : playerInventory.getArmor().keySet()) {
+      inventory.setStack(index, transferInventoryToItemStack(playerInventory.getArmor().get(index)));
+    }
+    // set offhand
+    for (int index : playerInventory.getOffhand().keySet()) {
+      inventory.setStack(index, transferInventoryToItemStack(playerInventory.getOffhand().get(index)));
+    }
+  }
+
   public static String enderChestInventoryAsJSON(EnderChestInventory inventory) {
     HashMap<Integer, InventoryItems> mainItemsMap = (HashMap<Integer, InventoryItems>) serializeItemStackList(inventory.heldStacks);
 
@@ -45,6 +80,29 @@ public class InventoryTransfer {
     } catch (JsonProcessingException e) {
       TheCoins.LOGGER.error("Cannot encode the player inventory from NBT tags");
       throw new RuntimeException(e);
+    }
+  }
+
+  public static void setEnderChestInventoryFromJSON(String ECInventoryString, EnderChestInventory inventory) {
+    // parse inventory JSON
+    ObjectMapper objectMapper = new ObjectMapper();
+    HashMap<Integer, InventoryItems> enderChestInventory;
+    try {
+      enderChestInventory = objectMapper.readValue(
+          ECInventoryString,
+          objectMapper.getTypeFactory().constructMapType(HashMap.class, Integer.class, InventoryItems.class)
+      );
+    } catch (JsonProcessingException e) {
+      TheCoins.LOGGER.error("Cannot get the inventory of player " + e);
+      throw new RuntimeException(e);
+    }
+
+    // clear
+    inventory.clear();
+
+    // set
+    for (int index : enderChestInventory.keySet()) {
+      inventory.setStack(index, transferInventoryToItemStack(enderChestInventory.get(index)));
     }
   }
 
@@ -64,12 +122,32 @@ public class InventoryTransfer {
           serializedItems.put(i, new InventoryItems(
               Registries.ITEM.getId(itemStack.getItem()).toString(),
               itemStack.getCount(),
-              null) // no NBT at all
+              "{}") // no NBT at all
           );
         }
       }
     }
 
     return serializedItems;
+  }
+
+  // 反序列化 InventoryItems to ItemStack
+  private static ItemStack transferInventoryToItemStack(InventoryItems inventoryItems) {
+    // item type
+    ItemStack itemStack = new ItemStack(Registries.ITEM.get(new Identifier(inventoryItems.getItemID())));
+
+    // item count
+    itemStack.setCount(inventoryItems.getCount());
+
+    // item NBT
+    try {
+      NbtCompound nbtCompound = NbtHelper.fromNbtProviderString(inventoryItems.getItemNBT());
+      itemStack.setNbt(nbtCompound);
+    } catch (CommandSyntaxException e) {
+      TheCoins.LOGGER.error("Cannot read the NBT from String " + inventoryItems.getItemID() + inventoryItems.getItemNBT() + e);
+      throw new RuntimeException(e);
+    }
+
+    return itemStack;
   }
 }
